a77fe787ce982b6fda73c40723a0da6c2de014e7
bielupc <biel.altimira@estudiantat.upc.edu>
2024-08-07 21:00:56 +0200

    Custom button component created

---
+++ front_sk/src/lib/components/Button.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+    export let className: string = "";
+    export let plain: true | false = false;
+    export let outline: true | false = false;
+    export let onClick: () => void = () => {};
+
+</script>
+
+<button on:click={onClick} class={`font-title font-bold text-[20px] border-accent border-2 rounded-[13px] px-[20px] py-[6px]
+                ${outline ? "bg-white text-accent border-accent" : "bg-accent text-white"}
+                ${className}`}
+        class:effect={!plain}>
+    <slot></slot>
+</button>
+
+<style>
+    .effect:hover{
+        box-shadow: 0 0;
+        cursor: pointer;
+        transform: translate(5px,5px) !important;
+    }
+    .effect{
+        box-shadow: 5px 5px #adadad;
+        text-decoration: none;
+        transition: transform .3s,box-shadow .3s;
+    }
+
+</style>
