4263d7cc5cca5da700da8a860d30bfe9d7148961
bielupc <biel.altimira@estudiantat.upc.edu>
2024-08-09 21:27:41 +0200

    favicon, tab title and hero section title

--- front_sk/src/routes/+page.svelte
+++ front_sk/src/routes/+page.svelte
@@ -6,8 +6,12 @@ import Header from '$lib/components/sections/Header.svelte';
 <div class="flex flex-col items-center h-screen">
     <Header />
 
-    <div class="h-[150px]"></div>
-    <h1 class="font-title font-bold text-[96px] text-dark text-center">Datathon FME</h1>
-    <h2 class="font-body text-[36px] text-secondary text-center">Join the competition now!</h2>
-    <Button className="mt-8">Register</Button>
+    <!-- Title -->
+    <div class="flex flex-col justify-center items-center h-[60vh]">
+        <h1 class="font-title font-bold  text-[62px] md:text-[96px]  text-dark text-center">Datathon FME</h1>
+        <h2 class="font-body text-[20px] md:text-[36px] text-secondary text-center">Join the competition now!</h2>
+        <Button className="mt-8">Register</Button>
+    </div>
+
+
 </div>
