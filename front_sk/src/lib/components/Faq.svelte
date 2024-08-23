baeec4b49e3d062acf7efd0adab64e5ef8fa413c
bielupc <biel.altimira@estudiantat.upc.edu>
2024-08-23 13:22:13 +0200

    updated faqs and responsiveness

--- front_sk/src/lib/components/Faq.svelte
+++ front_sk/src/lib/components/Faq.svelte
@@ -1,5 +1,4 @@
 <script>
-
   import plus from "$lib/assets/plus.svg";
   import minus from "$lib/assets/minus.svg";
 
@@ -7,20 +6,51 @@
   let toggle = false;
 </script>
 
-
+<style>
+.plusminus {
+	position: relative;
+	width: 20px;
+	height: 20px;
+	cursor: pointer;
+	
+	&.active {
+		&:before {
+			transform: translatey(-50%) rotate(-90deg);
+			opacity: 0;
+		}
+		&:after {
+			transform: translatey(-50%) rotate(0);
+		}
+	}
+	
+	&:before , &:after {
+		content: "";
+		display: block;
+		background-color: #21918c;
+		position: absolute;		
+		top: 50%; left: 0;
+		transition: .35s;
+		width: 100%;
+		height: 3px;
+	}
+	
+	&:before {		
+		transform: translatey(-50%);
+	}
+	
+	&:after {
+		transform: translatey(-50%) rotate(90deg);
+	}
+}
+</style>
 
 <li>
-    <div class="flex flex-row justify-between">
-        <h5 class="text-dark font-title text-[20px] font-bold">{question.question}</h5>
-        <button on:click={() => toggle = !toggle} class="text-accent font-title text-[36px] p-0 m-0">
-            {#if toggle}
-                <img src={minus}  class="h-6" alt="plus icon"/>
-            {:else}
-                <img src={plus}  class="h-6" alt="plus icon"/>
-            {/if}
-        </button>
+  <button on:click={() => toggle = !toggle} class="flex flex-row justify-between w-full text-left items-center">
+    <h5 class="text-dark font-title text-[20px] font-bold pr-4">{question.question}</h5>
+    <div class="text-accent font-title text-[36px] p-0 m-0 icon-container {toggle ? 'toggle' : ''}">
+        <div class=" plusminus {toggle ? "active" : ''}"></div>
     </div>
-    <div class="w-full bg-accent h-[2px] rounded-full mt-1"></div>
-    <p class={`text-dark font-body text-[16px] mt-1 ${toggle ? "block" : "hidden"}`}>{question.answer}</p>
+  </button>
+  <div class="w-full bg-accent h-[2px] rounded-full mt-1"></div>
+  <p class={`text-dark font-body text-[16px] mt-1 ${toggle ? "block" : "hidden"}`}>{question.answer}</p>
 </li>
-
--- front_sk/src/lib/components/sections/Faqs.svelte
+++ front_sk/src/lib/components/sections/Faqs.svelte
@@ -1,17 +1,15 @@
 <script>
     import {faqs} from '$lib/assets/faqs.json';
     const section = faqs[0];
-    console.log(section);
     import Faq from "$lib/components/Faq.svelte";
     import Card from "$lib/components/Card.svelte";
-
 </script>
 
 <div class=" w-full flex flex-col items-center pb-40 mt-52">
     <h2 class="font-title font-bold md:text-[48px] text-[36px] mb-10  text-dark text-center">FAQs</h2>
 
     {#each faqs as section}
-        <Card className="w-[70%]  bg-terc p-12 flex flex-col mb-20">
+        <Card className="w-[90%] sm:w-[70%]  bg-terc p-12 flex flex-col mb-20">
             <h6 class="text-accent font-title text-[16px] font-bold">{section.section_title}</h6>
 
             <ul class="pt-4 space-y-6">
