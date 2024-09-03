306a05d80760ac31a742a69a376e65ea49466ae3
laia <laiamogasp@gmail.com>
2024-09-03 23:50:36 +0200

    change subtitle

--- front_sk/src/lib/components/sections/Title.svelte
+++ front_sk/src/lib/components/sections/Title.svelte
@@ -7,7 +7,7 @@
 
 <div class="flex flex-col justify-center items-center my-40">
     <h1 class="font-title font-bold text-[62px] md:text-[96px] text-dark text-center">Datathon FME</h1>
-    <h4 class="font-body text-[20px] md:text-[36px] text-secondary text-center">Join the competition now!</h4>
+    <h4 class="font-body text-[20px] md:text-[36px] text-secondary text-center">Barcelona, November 16 - 17</h4>
     <Button className="mt-8">Register</Button>
 </div>
 <Logos />
