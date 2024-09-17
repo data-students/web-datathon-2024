4750083e45f38c85d51de116ff2afb90cea57ff9
bielupc <biel.altimira@estudiantat.upc.edu>
2024-09-17 18:15:24 +0200

    fixed aed section spacing

--- front_sk/src/lib/components/sections/Aed.svelte
+++ front_sk/src/lib/components/sections/Aed.svelte
@@ -6,11 +6,11 @@
 
 <div id="aed" class="flex justify-center items-center w-full mt-52">
   <!-- <div class="flex flex-col gap-24 lg:gap-0 lg:flex-row w-full lg:justify-evenly"> -->
-  <div class="w-full flex flex-col gap-24 min-[1500px]:flex-row min-[1500px]:justify-center min-[1500px]:space-x-20">
+  <div class="w-full flex flex-col gap-24 min-[1300px]:flex-row min-[1300px]:justify-center min-[1300px]:space-x-20">
 
-    <section class="flex flex-col min-[1500px]:flex-row items-center text-center min-[1500px]:text-left mb-8 min-[1500px]:mb-0 p-6 sm:p-0 ">
-      <img src={aedLogo} alt="About us icon" class="mb-4 min-[1500px]:mb-0 min-[1500px]:mr-6 h-auto w-24" />
-      <div class="flex flex-col items-center min-[1500px]:items-start max-w-sm">
+    <section class="flex flex-col min-[1300px]:flex-row items-center text-center min-[1300px]:text-left mb-8 min-[1300px]:mb-0 p-6 sm:p-0 ">
+      <img src={aedLogo} alt="About us icon" class="mb-4 min-[1300px]:mb-0 min-[1300px]:mr-6 h-auto w-24" />
+      <div class="flex flex-col items-center min-[1300px]:items-start max-w-sm md:max-w-2xl min-[1300px]:max-w-sm">
         <h2 class="font-title font-bold text-[36px] md:text-[48px] text-dark">About us</h2>
         <p class="text-secondary font-body mb-6 text-justify text-[14px] md:text-[17px] ">
           We are the Data Students' Association! We are organizing the fourth edition of this datathon, jointly with FME. Among our goals, we aim to promote data science within UPC. If you are studying <a href="https://dse.upc.edu/" target="_blank" class="text-accent decoration-accent underline">Data Science & Engineering</a>, you should definitely join us!
@@ -21,9 +21,9 @@
       </div>
     </section>
 
-    <section class="flex flex-col min-[1500px]:flex-row items-center text-center min-[1500px]:text-left  p-6 sm:p-0">
-      <img src={blackLogo} alt="Join the event icon" class="mb-4 min-[1500px]:mb-0 min-[1500px]:mr-6 h-auto w-24" />
-      <div class="flex flex-col items-center min-[1500px]:items-start max-w-sm">
+    <section class="flex flex-col min-[1300px]:flex-row items-center text-center min-[1300px]:text-left  p-6 sm:p-0">
+      <img src={blackLogo} alt="Join the event icon" class="mb-4 min-[1300px]:mb-0 min-[1300px]:mr-6 h-auto w-24" />
+      <div class="flex flex-col items-center min-[1300px]:items-start max-w-sm md:max-w-2xl min-[1300px]:max-w-sm">
         <h2 class="font-title font-bold text-[36px] md:text-[48px] text-dark">Join the event!</h2>
         <p class="text-secondary font-body mb-6 text-justify text-[14px] md:text-[17px]">
           If you are thinking of partnering/sponsoring us, don’t hesitate to reach us via email. If you have any doubts, feel free to contact us as well! We also accept suggestions or ideas for other events. We’re currently recruiting mentors and volunteers! Join us, it'll be fun!
