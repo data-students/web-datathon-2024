d1e17c749b7141d04febcfa386ee42b60c02b397
bielupc <biel.altimira@estudiantat.upc.edu>
2024-09-17 19:30:13 +0200

    added volunteer seeking section

--- front_sk/src/lib/components/sections/Aed.svelte
+++ front_sk/src/lib/components/sections/Aed.svelte
@@ -2,6 +2,7 @@
   import Button from "../Button.svelte";
   import aedLogo from '$lib/assets/aedLogo.png';
   import blackLogo from '$lib/assets/blackLogo.png';
+  import circle from '$lib/assets/circle.png';
 </script>
 
 <div id="aed" class="flex justify-center items-center w-full mt-52">
@@ -26,17 +27,41 @@
       <div class="flex flex-col items-center min-[1300px]:items-start max-w-sm md:max-w-2xl min-[1300px]:max-w-sm">
         <h2 class="font-title font-bold text-[36px] md:text-[48px] text-dark">Join the event!</h2>
         <p class="text-secondary font-body mb-6 text-justify text-[14px] md:text-[17px]">
-          If you are thinking of partnering/sponsoring us, don’t hesitate to reach us via email. If you have any doubts, feel free to contact us as well! We also accept suggestions or ideas for other events. We’re currently recruiting mentors and volunteers! Join us, it'll be fun!
+          If you are thinking of partnering/sponsoring us, don’t hesitate to reach us via email. If you have any doubts, feel free to contact us as well! We also accept suggestions or ideas for other events. 
         </p>
         <div class="flex flex-wrap space-x-1 align-stretch sm:space-x-4 justify-center">
           <a href="https://aed.cat/" target="_blank" class="mb-4">
             <Button outline>Contact us</Button>
           </a>
-          <a href="https://aed.cat/" target="_blank">
-            <Button outline>Volunteer</Button>
-          </a>
         </div>
       </div>
     </section>
   </div>
+  
 </div>
+  
+<div class="relative mt-20 md:mt-40 p-6 sm:p-0">
+  <!-- <img src={circle} alt="circle" class="absolute left-0 object-cover -z-20 w-auto h-[150%] w-[150%]" /> -->
+  <section class="relative flex flex-col min-[1300px]:flex-row items-center text-center min-[1300px]:text-left ">
+    <!-- Positioned absolutely within the section to act as a background -->
+    
+    <!-- Content of the section -->
+    <div class="flex flex-col items-center max-w-sm md:max-w-2xl  z-10">
+      <h2 class=" u font-title font-bold text-[36px] md:text-[48px] text-accent text-center">We're looking for volunteers!</h2>
+      <p class="text-secondary font-body mb-6 mt-2 text-justify text-[14px] md:text-[17px]">
+        If you are thinking of partnering/sponsoring us, don’t hesitate to reach us via email. If you have any doubts, feel free to contact us as well! We also accept suggestions or ideas for other events. We’re currently recruiting mentors and volunteers! Join us, it'll be fun!
+      </p>
+      <div class="flex flex-wrap space-x-1 sm:space-x-4 justify-center">
+        <a href="https://aed.cat/" target="_blank">
+          <Button outline>Volunteer</Button>
+        </a>
+      </div>
+    </div>
+  </section>
+</div>
+
+<style>
+  .u {
+    text-decoration: #21918C wavy underline;
+}
+</style>
