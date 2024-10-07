b9c8fd6a266832caf7ba0882c27e0f4ec1a0dd81
bielupc <biel.altimira@estudiantat.upc.edu>
2024-10-07 19:15:55 +0200

    remove bold

--- front_sk/src/lib/components/sections/Contact.svelte
+++ front_sk/src/lib/components/sections/Contact.svelte
@@ -8,15 +8,15 @@ import Card from "$lib/components/Card.svelte";
   </h2>
   <div class="bg-terc w-full text-dark p-6 sm:p-8 md:p-10 flex flex-col mr-0 sm:mr-40 rounded-[16px]">
     <h5 class="text-accent font-title text-[16px] sm:text-[18px] md:text-[20px] font-bold pr-4">General</h5>
-    <a href="mailto:hola@aed.cat" class="font-body text-[16px] sm:text-[18px] md:text-[20px] font-bold pr-4">hola@aed.cat</a>
+    <a href="mailto:hola@aed.cat" class="font-body text-[16px] sm:text-[18px] md:text-[20px]  pr-4">hola@aed.cat</a>
     <div class="w-full bg-accent h-[2px] rounded-full mt-1"></div>
     
     <h5 class="pt-10 text-accent font-title text-[16px] sm:text-[18px] md:text-[20px] font-bold pr-4">Sponsors</h5>
-    <a href="mailto:sponsors.datathon@aed.cat" class="font-body text-[16px] sm:text-[18px] md:text-[20px] font-bold pr-4">sponsors.datathon@aed.cat</a>
+    <a href="mailto:sponsors.datathon@aed.cat" class="font-body text-[16px] sm:text-[18px] md:text-[20px] pr-4">sponsors.datathon@aed.cat</a>
     <div class="w-full bg-accent h-[2px] rounded-full mt-1"></div>
     
     <h5 class="pt-10 text-accent font-title text-[16px] sm:text-[18px] md:text-[20px] font-bold pr-4">Registrations</h5>
-    <a href="mailto:registrations.datathon@aed.cat" class="font-body text-[16px] sm:text-[18px] md:text-[20px] font-bold pr-4">registrations.datathon@aed.cat</a>
+    <a href="mailto:registrations.datathon@aed.cat" class="font-body text-[16px] sm:text-[18px] md:text-[20px] pr-4">registrations.datathon@aed.cat</a>
     <div class="w-full bg-accent h-[2px] rounded-full mt-1"></div>
   </div>
 </div>
